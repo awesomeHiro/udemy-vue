c976f42c4e9bba63d956c74112805b65b794d1b2
awesomeHiro <48130097+awesomeHiro@users.noreply.github.com>
2020-04-10 05:15:54 +0900

    init

---
+++ vue-cli/src/components/User.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm {{ name }}!</p>
+    <p>{{ age }} years old!</p>
+    <button @click="reverseName">reverseName</button>
+    <hr />
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <app-user-detail
+          :name="name"
+          :age="age"
+          @nameChanged="name = $event"
+         ></app-user-detail>
+        <!-- <app-user-detail></app-user-detail> -->
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit :name="name" :age="age" @ageChanged="age = $event"></app-user-edit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserDetail from "./UserDetail.vue";
+import UserEdit from "./UserEdit.vue";
+
+export default {
+  data: function () {
+    return {
+      name: "Max",
+      age: 24
+    };
+  },
+  methods: {
+    reverseName() {
+      this.name = this.name.split("").reverse().join("");
+    },
+    resetName() {
+      this.name = 'Max'
+    }
+  },
+  components: {
+    appUserDetail: UserDetail,
+    appUserEdit: UserEdit,
+  },
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightblue;
+}
+</style>
---
+++ vue-cli/src/components/UserEdit.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="component">
+    <h3>Edit</h3>
+    <p>name: {{ name }}</p>
+    <p>age: {{ age }}</p>
+    <button @click="editAge">EditAge</button>
+  </div>
+</template>
+
+<script>
+  export default{
+    props:{
+      name:{},
+      age:{}
+    },
+    methods:{
+      editAge(){
+        this.age = 30
+        this.$emit('ageChanged', this.age)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+div {
+  background-color: lightgreen;
+}
+</style>
