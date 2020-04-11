f429eaecb36920b3820e52fb0ad1e9ec8e49ee61
awesomeHiro <48130097+awesomeHiro@users.noreply.github.com>
2020-04-11 15:46:57 +0900

    ready to develop

--- quote/src/App.vue
+++ quote/src/App.vue
@@ -5,52 +5,37 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <h1>Quote</h1>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <h1>Awesome</h1>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <Header/>
+      <v-divider></v-divider>
+      <Add/>
+  <v-divider></v-divider>
+      <List/>
+  <v-divider></v-divider>
+      <Footer/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Header from './components/Header';
+import Add from './components/Add';
+import List from './components/List';
+import Footer from './components/Footer';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Header,
+    Add,
+    List,
+    Footer
   },
 
   data: () => ({
