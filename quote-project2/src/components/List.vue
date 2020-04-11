8c183b596425f1adb42912811c62fd7c21dbb48b
awesomeHiro <48130097+awesomeHiro@users.noreply.github.com>
2020-04-11 15:15:17 +0900

    add quote project with vuetify

--- quote-project2/src/components/List.vue
+++ quote-project2/src/components/List.vue
@@ -1,12 +1,32 @@
 <template>
     <div class="container">
         <h1>list</h1>
+        <ul id="example-1">
+            <li v-for="quote in quotes" :key="quote">
+                {{ quote }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data: function(){
+            return {
+                quotes: [
+                    'Quote1',
+                    'Quote2',
+                    'Quote3',
+                    'Quote4',
+                    'Quote5',
+                    'Quote6',
+                    'Quote7',
+                    'Quote8',
+                    'Quote9',
+                    'Quote10',
+                ]
+            }
+        }
     }
 </script>
 
