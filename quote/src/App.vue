1008141d6a7bbc82ab849ff55142e71cf546d04f
awesomeHiro <48130097+awesomeHiro@users.noreply.github.com>
2020-04-11 15:55:55 +0900

    use v-content

--- quote/src/App.vue
+++ quote/src/App.vue
@@ -1,35 +1,33 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <h1>Quote</h1>
       <v-spacer></v-spacer>
       <h1>Awesome</h1>
     </v-app-bar>
 
     <v-content>
-      <Header/>
-      <v-divider></v-divider>
-      <Add/>
-  <v-divider></v-divider>
-      <List/>
-  <v-divider></v-divider>
-      <Footer/>
+      <v-container fluid fill-wide>
+        <Header />
+        <v-divider></v-divider>
+        <Add />
+        <v-divider></v-divider>
+        <List />
+        <v-divider></v-divider>
+        <Footer />
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import Header from './components/Header';
-import Add from './components/Add';
-import List from './components/List';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Add from "./components/Add";
+import List from "./components/List";
+import Footer from "./components/Footer";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
     Header,
@@ -40,6 +38,6 @@ export default {
 
   data: () => ({
     //
-  }),
+  })
 };
 </script>
