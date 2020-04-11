5c4d6fae160ddfe5d532f2f7275945459353bb3f
awesomeHiro <48130097+awesomeHiro@users.noreply.github.com>
2020-04-11 14:58:37 +0900

    new project

---
+++ quote-project2/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <cheader></cheader>
+        <hr>
+        <cadd></cadd>
+        <hr>
+        <clist></clist>
+        <hr>
+        <cfooter></cfooter>
+    </div>
+</template>
+
+<script>
+import cheader from "./components/Header.vue"
+import cadd from "./components/Add.vue"
+import clist from "./components/List.vue"
+import cfooter from "./components/Footer.vue"
+    export default {
+        components: {
+            cheader,
+            cadd,
+            clist,
+            cfooter
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ vue-cli/src/components/New.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h3>New</h3>
+        <p>{{conunter}}</p>
+        <button @click="conunter++">INCREASE!</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data:function(){
+            return {
+                conunter: 0
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
